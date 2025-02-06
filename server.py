6e51bbfff3aaadd87bbb9ee7f90c23efa1590a74
Uniunt🍥⚓ <106765520+Uniunt@users.noreply.github.com>
2025-02-06 17:15:09 +0800

    Initial commit of Python backend

--- server.py
+++ server.py
@@ -72,6 +72,8 @@ def get_movies():
     return jsonify(movies)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # 从环境变量获取端口
+    app.run(host='0.0.0.0', port=port)
+    # app.run(debug=True)
 
 
